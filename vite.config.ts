eb791d51a512b07a054ea641e33bd67fa9c7c943
Cemre <cemredisli@hotmail.com>
2025-09-21 14:39:29 +0300

    vite fixes for railway

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Backend domain Railway 
+const BACKEND_URL = 'https://aeta-production.up.railway.app'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:8080",
+      // development, localhost
+      '/api': {
+        target: BACKEND_URL, // HTTPS Railway domain
         changeOrigin: true,
-         },
+        secure: true, // because HTTPS 
+      },
     },
   },
-});
+})
